f30375900b05af58c251ecf325eec6def3508ed6
Diego <diegoblom01@gmail.com>
2022-06-05 10:53:38 +0200

    change

--- savedPhrases.py
+++ savedPhrases.py
@@ -1,41 +1,38 @@
-from ast import keyword
-from distutils.log import error
-import string
+from email.mime import audio
 import speech_recognition as sr
 import turtle
 import time
+
 
 wn = turtle.Screen()
 wn.title("Responsive test")
 wn.bgcolor("grey")
 
 
-def main():
+def takecommand():
     r = sr.Recognizer()
-    keyword = 'hello'
-
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-
-        turtle.write("I am all ears... \n")
-
+        turtle.write("Waiting for command...(Hello)/(GoodBye)")
         audio = r.listen(source)
-
-        try:
-            if r.recognize_google(audio) == keyword:
-                turtle.write("Hello! \n")
-
-            elif r.recognize_google(audio) is not keyword:
-                turtle.write("I am sorry, I don't understand.. :(")
-
-            turtle.write("You have said \n" +
-                         r.recognize_google(audio, language="en-us"))
-
-        except Exception as e:
-            print("Error : " + str(e))
-
+    try:
+        turtle.write("Recognizing...")
+        query = r.recognize_google(audio, language='en')
+        turtle.write(f"You said: {query}")
         with open("recordedaudio.wav", "wb") as f:
             f.write(audio.get_wav_data())
+    except Exception as e:
+        return "none"
+    query = query.lower()
+    return query
+
+
+def main():
+    querry = takecommand()
+
+    if "Hello" in querry:
+        turtle.write("Hello Sir!")
+    else:
+        pass
 
 
 if __name__ == "__main__":
